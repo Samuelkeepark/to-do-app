13c4e1cf3ca6b1ad20a4da170d04905bece75ecc
Samuel Park <samuelkeepark@gmail.com>
2018-07-15 14:58:50 -0400

    Redid assignment

--- app.js
+++ app.js
@@ -16,10 +16,22 @@ function onReady () {
     let checkbox = document.createElement('input');
 
     // set the input's type to checkbox
-    checkbox.type = "checkbox";
+    checkbox.type = 'checkbox';
 
     // set the title
     newLi.textContent = title;
+
+    // set button text
+    deleteButton.textContent = 'Delete';
+
+    // newLi style
+    newLi.classList.add("mdl-list__item-primary-content");
+
+    // checkbox style
+    checkbox.classList.add("check");
+
+    // deleteButton
+    deleteButton.classList.add("mdl-button", "mdl-js-button". "mdl-button--raised");
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
@@ -30,10 +42,23 @@ function onReady () {
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
-    //empty the input
+    // empty the input
     newToDoText.value = '';
-  });
-}
+
+    // create delete button
+    let deletebutton = document.createElement('button');
+
+    // set button text
+    deleteButton.textContent = 'Delete';
+
+    deleteButton.onclick = function() {
+      toDoList.removeChild(newLi);
+    }
+    });
+
+    // attach delete button to newLi
+    newLi.appendChild(deleteButton);
+  };
 
 window.onload = function() {
   onReady();
