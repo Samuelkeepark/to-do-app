0c2c4d53624bb9bc601e78fa0af4dbbcddc9aafd
Samuel Park <samuelkeepark@gmail.com>
2018-07-16 19:49:52 -0400

    original master version

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 function onReady() {
-<<<<<<< HEAD
-=======
-  const toDos=[];
->>>>>>> dom-two
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-<<<<<<< HEAD
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
     // get the text
     let title = newToDoText.value;
-=======
-  function createNewToDo() {
-    const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) {return;}
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-    });
-    newToDoText.value = '';
->>>>>>> dom-two
 
     // create a new li
     let newLi = document.createElement('li');
@@ -39,27 +24,15 @@ function onReady() {
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
-<<<<<<< HEAD
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
     // empty the input
     newToDoText.value = '';
-=======
-      toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
-    });
-  }
->>>>>>> dom-two
 
   });
 }
 
-<<<<<<< HEAD
-=======
-  renderTheUI();
-}
->>>>>>> dom-two
 
 window.onload = function() {
   onReady();
